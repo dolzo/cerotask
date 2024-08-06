0d7a805340e63efe70a65937b14d00f2d75513d3
dolzo <degolxz01@gmail.com>
2024-08-06 14:05:47 -0400

    fix: Cambiado el valor de rol de usuario a role_admin en middleware checkAdminRole

--- middleware/checkAdminRole.js
+++ middleware/checkAdminRole.js
@@ -1,7 +1,7 @@
 // Middleware para comprobar rol de admin
 const checkAdminRole = (req, res, next) => {
     const userRole = req.user.role;
-    if (userRole != 'user_admin') {
+    if (userRole != 'role_admin') {
         return res.status(403).send({
             status: 'error',
             message: 'Acceso no autorizado',
