c663d1cd5ef933dcd247da1595cb7eb44b6e683e
dolzo <degolxz01@gmail.com>
2024-08-08 15:05:00 -0400

    fix:: arreglando la ruta para listar una tarea especifica

--- routes/task.js
+++ routes/task.js
@@ -12,6 +12,6 @@ const task = require('../controllers/task');
 // Rutas
 router.post('/create-task', auth, task.createTask);
 router.get('/tasks', auth, checkAdminRole, task.getTasks);
-router.get('/tasks:id', auth, task.getSpecificTask);
+router.get('/tasks/:id', auth, task.getSpecificTask);
 
 module.exports = router;
