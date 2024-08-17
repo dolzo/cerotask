09dd17e29602f0d2c06279865aa969da552a0398
dolzo <degolxz01@gmail.com>
2024-08-17 12:16:06 -0400

    refactor: cambiando la ruta del enpoint que lista a todos los usuario con paginacion

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ const user = require('../controllers/user');
 // Rutas
 router.post('/create-user', user.createUser);
 router.post('/login', user.loginUser);
-router.get('/users', auth, checkAdminRole, user.getUsers);
+router.get('/users/page/:page', auth, checkAdminRole, user.getUsers);
 router.get('/users/:id', auth, checkAdminRole, user.getSpecificUser);
 router.put('/update-user/', auth, user.updateUser);
 router.delete('/delete/:id', auth, user.deleteUser);
